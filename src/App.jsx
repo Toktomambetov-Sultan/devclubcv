a0a42e461ec87419d10206af6aa8293824f35b4e
Toktomambetov-Sultan <sultan.toktomambetov.04@bk.ru>
2025-01-01 23:04:23 +0600

    added rest of the projects

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,25 @@
-import './App.css'
-import Header from 'pages/Header'
-import GeneralPurpose from 'pages/GeneralPurpose'
-import Projects from 'pages/Projects'
-import backImage from "assets/licensed-image.jpeg"
+import './App.css';
+import Header from 'pages/Header';
+import GeneralPurpose from 'pages/GeneralPurpose';
+import Projects from 'pages/Projects';
+import backImage from 'assets/licensed-image.jpeg';
 
 function App() {
-
-  return (
-    <>
-      <Header />
-      <div style={{
-        backgroundImage: `url(${backImage})`
-      }}>
-
-        <GeneralPurpose />
-        <Projects />
-      </div>
-    </>
-  )
+    return (
+        <>
+            <Header />
+            <div
+                style={{
+                    backgroundImage: `url(${backImage})`,
+                    backgroundRepeat: 'no-repeat',
+                    backgroundColor: "black"
+                }}
+            >
+                <GeneralPurpose />
+                <Projects />
+            </div>
+        </>
+    );
 }
 
-export default App
+export default App;
